d278059bdc818b194fd4a7af9b46f4e44e87eb3c
mwestin <marcus@sekurbit.se>
2014-06-30 13:52:47 +0200

    added support for resuming suspended runbooks

--- SMAStudio/Commands/ResumeCommand.cs
+++ SMAStudio/Commands/ResumeCommand.cs
@@ -1,4 +1,5 @@
-﻿using SMAStudio.Services;
+﻿using SMAStudio.Util;
+using SMAStudio.Services;
 using SMAStudio.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -20,15 +21,21 @@ namespace SMAStudio.Commands
 
         public bool CanExecute(object parameter)
         {
-            if (!(parameter is RunbookViewModel))
+            if (!(parameter is RunbookViewModel) && !(parameter is ExecutionViewModel))
                 return false;
 
-            var runbook = (RunbookViewModel)parameter;
+            RunbookViewModel runbook = null;
+            if (parameter is RunbookViewModel)
+                runbook = (RunbookViewModel)parameter;
+            else
+                runbook = ((ExecutionViewModel)parameter).Runbook;
+
             Guid jobGuid = Guid.Empty;
 
-            if ((jobGuid = _runbookService.GetSuspendedJobs(runbook)) != Guid.Empty)
+            if ((jobGuid = _runbookService.GetSuspendedJobs(runbook.Runbook)) != Guid.Empty)
             {
                 runbook.JobID = jobGuid;
+                return true;
             }
 
             return false;
@@ -42,7 +49,35 @@ namespace SMAStudio.Commands
 
         public void Execute(object parameter)
         {
-            // TODO: Implement resuming
+            if (!(parameter is RunbookViewModel) && !(parameter is ExecutionViewModel))
+                return;
+
+            RunbookViewModel runbook = null;
+            if (parameter is RunbookViewModel)
+                runbook = (RunbookViewModel)parameter;
+            else
+                runbook = ((ExecutionViewModel)parameter).Runbook;
+
+            var api = Core.Resolve<IApiService>();
+            var job = api.Current.Jobs.Where(j => j.JobID.Equals(runbook.JobID)).FirstOrDefault();
+            
+            if (job == null)
+            {
+                Core.Log.WarningFormat("Resume Runbook: The given JobID was not found in SMA anymore. Either this job has been resumed outside of SMA Studio or there is some kind of connectivity issue. Please try again later.");
+                return;
+            }
+
+            job.Resume(api.Current);
+
+            // If we execute from the ExecutionWindow, the parameter is of type ExecutionViewModel
+            // and don't want to open a new execution window.
+            if (parameter is RunbookViewModel)
+            {
+                var executionWindow = new ExecutionWindow(runbook);
+                executionWindow.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+                executionWindow.Show();
+            }
         }
     }
 }
